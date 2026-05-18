98e563fa69b80e63f62545ad2a57d598b5de743e
Clariveljn <cjeldres5@gmail.com>
2026-05-18 14:37:27 -0400

    Implementa login con JWT

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,18 +5,40 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+    return users.some(user => user.username === username);
+  };
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
-
+  const authenticatedUser = (username, password) => {
+    return users.some(user => user.username === username && user.password === password);
+  };
+  
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+  const password = req.body.password;
+
+  if(!username || !password){
+    return res.status(404).json({message:"Error en login"});
+  }
+
+  if(authenticatedUser(username,password)){
+
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken,
+      username
+    };
+
+    return res.status(200).json({
+      message:"Usuario autenticado correctamente"
+    });
+  }
+
+  return res.status(208).json({message:"Usuario o contraseña inválidos"});
 });
 
 // Add a book review
